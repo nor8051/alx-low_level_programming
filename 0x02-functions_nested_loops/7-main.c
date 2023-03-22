4ad5ba89bcbdca5f4cb6d8c8d0aee0f94dff3981
nor8051 <103929728+nor8051@users.noreply.github.com>
2023-03-23 00:30:00 +0200

    Update 7-main.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include "main.h"
 
 /**
@@ -7,13 +8,10 @@
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+	r = print_last_digit(98);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
 }
-
