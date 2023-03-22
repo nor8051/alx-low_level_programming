2d5ca8e5c996341e54f05db378244a18169a59cd
nor8051 <103929728+nor8051@users.noreply.github.com>
2023-03-23 00:28:42 +0200

    Update 7-main.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
-	
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)		
-{	
+int main(void)
+{
     int r;
 
     print_last_digit(98);
@@ -16,3 +16,4 @@ int main(void)
     _putchar('\n');
     return (0);
 }
+
