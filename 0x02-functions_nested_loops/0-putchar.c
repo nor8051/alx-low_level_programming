b3ab4d7bcbd218fe2e5f1383a75c06aa79e67b86
nor8051 <103929728+nor8051@users.noreply.github.com>
2023-03-22 23:42:46 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,9 @@
 #include "main.h"
 	/**
- * main - Entry point
- * 
- * Description: 'the program's description'
- * @parameter: describe the parameter
- * 
- * Return: Always 0 (Success)
- */
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
 
 
 	int main(void)
