3c614e258991004d7c3204bcef19c11b96f68c63
nor8051 <103929728+nor8051@users.noreply.github.com>
2023-03-23 20:07:04 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,29 @@
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
 
+
+1-main.c
+
+#include <stdio.h>
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
- int i;
-
- i = 98;
- positive_or_negative(i);
-
- return (0);
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
