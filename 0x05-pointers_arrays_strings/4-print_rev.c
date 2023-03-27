56db507fcccc411e0d93150855d17b27a68b85b2
nor8051 <103929728+nor8051@users.noreply.github.com>
2023-03-27 20:29:34 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,23 @@
 #include "main.h"
+#include "stdio.h"
 /**
- * print_rev - imprime en reversa
- * @s: string
- * return: 0
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
  */
-void print_rev(char *s)
+void print_array(int *a, int n)
 {
-	int longi = 0;
-	int o;
+	int j;
 
-	while (*s != '\0')
+	for (j = 0; j < (n - 1); j++)
 	{
-		longi++;
-		s++;
+		printf("%d, ", a[j]);
 	}
-	s--;
-	for (o = longi; o > 0; o--)
-	{
-		_putchar(*s);
-		s--;
-	}
-
-	_putchar('\n');
+		if (j == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
 }
+
